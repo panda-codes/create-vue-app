37a698d8d57f6a6ecff33721c66b5a9622ef202a
panda-codes <dedeomatse@gmail.com>
2024-10-24 19:47:11 +0100

    finished card component now working on job list as prop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script></script>
+<script setup>
+  import NavBar from './components/NavBar.vue';
+  import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
+  import JobListing from './components/JobListing.vue';
+</script>
 
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <NavBar/>
+  <Hero Title="Test Title" subtitle="Find the Vue job that fits your skills and needs"/>
+  <HomeCards/>
+  <JobListing/>
 </template>
 
 <style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps({
+        bg:{
+            type: String,
+            default:"bg-gray-100"
+        }
+    })
+</script>
+
+<template>
+    <div :class="`${bg} p-6 rounded-lg shadow-md`"><!--backticks within the double quotes to use javaScript/ :class to bind dynamic value-->
+        <slot></slot>
+    </div>
+</template>
